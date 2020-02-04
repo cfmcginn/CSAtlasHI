9d281d50c24f1f58277424d5453e4eba26baef45
Chris <cfmcginn@mit.edu>
2020-02-04 20:05:48 +0100

    Update for calculating weights on more arbitrary inputs, modeled after directory structure of berenice files for gamma+jets

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ lsetup "lcgenv -p LCG_96b x86_64-centos7-gcc8-opt fjcontrib" #FASTJET CONTRIB\
 lsetup "root 6.18.04-x86_64-centos7-gcc8-opt" #root6 distribution
 
 on a personal machine you will have to build these packages and link manually
+
+If running a macro complains 'error while loading shared libraries: libCSATLAS.so', try from the directory of the Makefile:\
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$PWD/lib
--- src/deriveSampleWeights.C
+++ src/deriveSampleWeights.C
@@ -5,6 +5,7 @@
 #include <iostream>
 #include <map>
 #include <string>
+#include <vector>
 
 //ROOT
 #include "TCanvas.h"
@@ -20,6 +21,7 @@
 #include "include/histDefUtility.h"
 #include "include/kirchnerPalette.h"
 #include "include/plotUtilities.h"
+#include "include/returnRootFileContentsList.h"
 #include "include/stringUtil.h"
 
 int deriveSampleWeights(std::string inXSectionFileName, std::string inEntriesFileName)
@@ -27,10 +29,13 @@ int deriveSampleWeights(std::string inXSectionFileName, std::string inEntriesFil
   checkMakeDir check;
   if(!check.checkFileExt(inXSectionFileName, ".txt")) return 1;
 
+  const std::string rootExt = ".root";
+  const std::string txtExt = ".txt";
+
   bool entriesFileTXT = true;
-  if(!check.checkFileExt(inEntriesFileName, ".txt")){
+  if(!check.checkFileExt(inEntriesFileName, txtExt)){
     entriesFileTXT = false;
-    if(!check.checkFileExt(inEntriesFileName, ".root")) return 1;
+    if(!check.checkFileExt(inEntriesFileName, rootExt)) return 1;
   }
 
   kirchnerPalette kPal;
@@ -43,11 +48,12 @@ int deriveSampleWeights(std::string inXSectionFileName, std::string inEntriesFil
   check.doCheckMakeDir("pdfDir");
   check.doCheckMakeDir("pdfDir/" + dateStr);
 
-  std::cout << "FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
-
   std::map<std::string, std::vector<double> > jzMapToXSec;
   std::map<std::string, double> jzMapToEntries;
   std::map<std::string, double> jzMapToWeights;
+  std::map<std::string, int> fileNameToJZVal;
+  std::map<std::string, std::string> fileNameToTTreeStr;
+
   std::ifstream inXSectionFile(inXSectionFileName.c_str());
   std::string tempStr;
   while(std::getline(inXSectionFile, tempStr)){
@@ -62,22 +68,87 @@ int deriveSampleWeights(std::string inXSectionFileName, std::string inEntriesFil
   }
   inXSectionFile.close();
 
-  std::cout << "FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
-
+  std::vector<std::vector<std::string> > globalTxtVect;
+  bool inIsROOT = false;
   if(entriesFileTXT){
-    std::cout << "FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
     std::ifstream inEntriesFile(inEntriesFileName.c_str());
+
     while(std::getline(inEntriesFile, tempStr)){
       if(tempStr.size() == 0) continue;
       std::vector<std::string> tempVect = commaSepStringToVect(tempStr);
       if(tempVect.size() == 0) continue;
       if(tempVect[0].size() == 0) continue;
       if(tempVect[0].substr(0,1).find("#") != std::string::npos) continue;
-      
-      jzMapToEntries[tempVect[0]] = std::stod(tempVect[1]);
+   
+      std::cout << tempVect[1] << std::endl;
+   
+      globalTxtVect.push_back(tempVect);
+      if(tempVect[1].rfind(".") != std::string::npos){
+	if(isStrSame(rootExt, tempVect[1].substr(tempVect[1].rfind("."), rootExt.size()))) inIsROOT = true;
+      }
+      //      jzMapToEntries[tempVect[0]] = std::stod(tempVect[1]);
     }
     inEntriesFile.close();
-    std::cout << "FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
+
+    std::cout << "ROOT " << inIsROOT << std::endl;
+
+    for(unsigned int gI = 0; gI < globalTxtVect.size(); ++gI){
+      if(!inIsROOT) jzMapToEntries[globalTxtVect[gI][0]] = std::stod(globalTxtVect[gI][1]); 
+      else{
+	if(!check.checkFileExt(globalTxtVect[gI][1], rootExt)) continue;
+	TFile* inFile_p = new TFile(globalTxtVect[gI][1].c_str(), "READ");
+	std::vector<std::string> treeList = returnRootFileContentsList(inFile_p, "TTree");
+	
+	if(treeList.size() == 0){
+	  std::cout << "DERIVESAMPLEWEIGHTS WARNING: FILE \'" << globalTxtVect[gI][1] << "\' contains zero TTree. continue" << std::endl;
+	  inFile_p->Close();
+	  delete inFile_p;
+	  continue;
+	}
+	else if(treeList.size() > 1){
+	  if(globalTxtVect[gI].size() < 3){
+	    std::cout << "DERIVESAMPLEWEIGHTS WARNING: FILE \'" << globalTxtVect[gI][1] << "\' contains more than 1 TTree and input txt file doesnt not disambiguate (specify as third arg in comma separated list). continue" << std::endl;
+	    inFile_p->Close();
+	    delete inFile_p;
+	    continue;
+	  }
+	  else{
+	    if(!vectContainsStr(globalTxtVect[gI][2], &treeList)){
+	      std::cout << "DERIVESAMPLEWEIGHTS ERROR: Specified TTree \'" << globalTxtVect[gI][2] << "\' not found in corresponse TFile \'" << globalTxtVect[gI][1] << "\'. return 1" << std::endl;
+	      inFile_p->Close();
+	      delete inFile_p;
+	      return 1;
+	    }
+	    else{
+	      TTree* tempTree_p = (TTree*)inFile_p->Get(globalTxtVect[gI][2].c_str());
+	      fileNameToTTreeStr[globalTxtVect[gI][1]] = globalTxtVect[gI][2];
+	      jzMapToEntries[globalTxtVect[gI][0]] += (double)tempTree_p->GetEntries();
+	    }	  
+	  }
+	}
+	else{
+	  TTree* tempTree_p = (TTree*)inFile_p->Get(treeList[0].c_str());
+	  fileNameToTTreeStr[globalTxtVect[gI][1]] = treeList[0];
+	  jzMapToEntries[globalTxtVect[gI][0]] += tempTree_p->GetEntries();
+	}
+	
+	if(isStrSame(globalTxtVect[gI][0], "JZ1")) fileNameToJZVal[globalTxtVect[gI][1]] = 1;
+	else if(isStrSame(globalTxtVect[gI][0], "JZ2")) fileNameToJZVal[globalTxtVect[gI][1]] = 2;
+	else if(isStrSame(globalTxtVect[gI][0], "JZ3")) fileNameToJZVal[globalTxtVect[gI][1]] = 3;
+	else if(isStrSame(globalTxtVect[gI][0], "JZ4")) fileNameToJZVal[globalTxtVect[gI][1]] = 4;
+	else if(isStrSame(globalTxtVect[gI][0], "JZ5")) fileNameToJZVal[globalTxtVect[gI][1]] = 5;
+	else if(isStrSame(globalTxtVect[gI][0], "JZ6")) fileNameToJZVal[globalTxtVect[gI][1]] = 6;
+	else{
+	  std::cout << "DERIVESAMPLEWEIGHTS ERROR: FILE NAME \'" << globalTxtVect[gI][1] << "\' contains no valid JZX. return 1" << std::endl;
+	  inFile_p->Close();
+	  delete inFile_p;
+	  return 1;
+	}
+	
+	inFile_p->Close();
+	delete inFile_p;
+      }
+    }   
   }
   else{
     std::cout << "FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
@@ -98,6 +169,9 @@ int deriveSampleWeights(std::string inXSectionFileName, std::string inEntriesFil
       std::string jzValStr = "JZ" + std::to_string(jzVal_);
       if(jzMapToEntries.count(jzValStr) == 0) std::cout << "WARNING MAP IS MISSING VALUE " << jzValStr << std::endl;
       ++(jzMapToEntries[jzValStr]);      
+
+      fileNameToTTreeStr[inEntriesFileName] = jzVal_;
+      fileNameToTTreeStr[inEntriesFileName] = "clusterJetsCS";
     }
 
     inFile_p->Close();
@@ -132,12 +206,16 @@ int deriveSampleWeights(std::string inXSectionFileName, std::string inEntriesFil
   std::cout << "FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
   
   std::string outFileName = "";
-  if(!entriesFileTXT){
-    outFileName = inEntriesFileName.substr(0, inEntriesFileName.rfind(".root"));
+  if(!entriesFileTXT || inIsROOT){
+    if(!entriesFileTXT) outFileName = inEntriesFileName.substr(0, inEntriesFileName.rfind(rootExt));
+    else outFileName = inEntriesFileName.substr(0, inEntriesFileName.rfind(txtExt));
+
     while(outFileName.find("/") != std::string::npos){
       outFileName.replace(0, outFileName.find("/")+1, "");
     }
-    outFileName = "output/" + dateStr + "/" + outFileName + "_DerivedWeights_" + dateStr + ".root";
+    outFileName = "output/" + dateStr + "/" + outFileName + "_DerivedWeights_" + dateStr + rootExt;
+
+    std::cout << "BUILDING WEIGHTS CANVAS" << std::endl;
     
     const Int_t nJtPtBins = 110;
     Float_t jtPtLow = 20.;
@@ -150,41 +228,76 @@ int deriveSampleWeights(std::string inXSectionFileName, std::string inEntriesFil
     TH1D* jtpt_Weighted_h = new TH1D("jtpt_Weighted_h", ";Jet p_{T} [GeV];Effective Counts (Weighted)", nJtPtBins, jtPtBins);
     centerTitles({jtpt_h, jtpt_Weighted_h});
 
-    TFile* inFile_p = new TFile(inEntriesFileName.c_str(), "READ");
-    TTree* inTree_p = (TTree*)inFile_p->Get("clusterJetsCS");
-    Int_t jzVal_;
+    //EDIT HERE CFM
+    TFile* inFile_p = nullptr;
+    std::vector<std::string> fileList;
+    if(!entriesFileTXT) fileList.push_back(inEntriesFileName);
+    else{
+      //fileNameToJZStr mpa
+
+      for(auto const & iter : fileNameToJZVal){
+	fileList.push_back(iter.first);
+      }
+    }
+
+    std::cout << "POST FILE LIST REWORK" << std::endl;
 
     const Int_t nMaxJets = 500;
     Int_t njtTruth_;
     Float_t jtptTruth_[nMaxJets];
 
-    inTree_p->SetBranchStatus("*", 0);
-    inTree_p->SetBranchStatus("jzVal", 1);
-    inTree_p->SetBranchStatus("njtTruth", 1);
-    inTree_p->SetBranchStatus("jtptTruth", 1);
+    std::vector<float>* jtptTruth_p=nullptr;
 
-    inTree_p->SetBranchAddress("jzVal", &jzVal_);
-    inTree_p->SetBranchAddress("njtTruth", &njtTruth_);
-    inTree_p->SetBranchAddress("jtptTruth", jtptTruth_);
-
-    const Int_t nEntries = inTree_p->GetEntries();
-
-    for(Int_t entry = 0; entry < nEntries; ++entry){
-      inTree_p->GetEntry(entry);
-
-      std::string jzValStr = "JZ" + std::to_string(jzVal_);
-      if(jzMapToEntries.count(jzValStr) == 0) std::cout << "WARNING MAP IS MISSING VALUE " << jzValStr << std::endl;
-
-      Double_t weight = jzMapToWeights[jzValStr];      
-
-      for(Int_t jI = 0; jI < njtTruth_; ++jI){
-	jtpt_h->Fill(jtptTruth_[jI]);
-	jtpt_Weighted_h->Fill(jtptTruth_[jI], weight);
+    for(unsigned int fI = 0; fI < fileList.size(); ++fI){
+      inFile_p = new TFile(fileList[fI].c_str(), "READ");
+      TTree* inTree_p = (TTree*)inFile_p->Get(fileNameToTTreeStr[fileList[fI]].c_str());
+      Int_t jzVal_;  
+      
+      inTree_p->SetBranchStatus("*", 0);
+      if(!entriesFileTXT){ 
+	inTree_p->SetBranchStatus("jzVal", 1);
+	inTree_p->SetBranchStatus("njtTruth", 1);
+	inTree_p->SetBranchStatus("jtptTruth", 1);
+      
+	inTree_p->SetBranchAddress("jzVal", &jzVal_);
+	inTree_p->SetBranchAddress("njtTruth", &njtTruth_);
+	inTree_p->SetBranchAddress("jtptTruth", jtptTruth_);
       }
-    }
+      else{
+	jzVal_ = fileNameToJZVal[fileList[fI]];
 
-    inFile_p->Close();
-    delete inFile_p;
+	inTree_p->SetBranchStatus("akt4_truth_jet_pt", 1);
+
+	inTree_p->SetBranchAddress("akt4_truth_jet_pt", &jtptTruth_p);
+      }
+
+      const Int_t nEntries = inTree_p->GetEntries();
+      
+      for(Int_t entry = 0; entry < nEntries; ++entry){
+	inTree_p->GetEntry(entry);
+
+	if(entriesFileTXT){
+	  njtTruth_ = 0;
+	  for(unsigned int jI = 0; jI < jtptTruth_p->size(); ++jI){
+	    jtptTruth_[njtTruth_] = (*jtptTruth_p)[jI];
+	    ++njtTruth_;
+	  }
+	}
+	
+	std::string jzValStr = "JZ" + std::to_string(jzVal_);
+	if(jzMapToEntries.count(jzValStr) == 0) std::cout << "WARNING MAP IS MISSING VALUE " << jzValStr << std::endl;
+
+	Double_t weight = jzMapToWeights[jzValStr];      
+	
+	for(Int_t jI = 0; jI < njtTruth_; ++jI){
+	  jtpt_h->Fill(jtptTruth_[jI]);
+	  jtpt_Weighted_h->Fill(jtptTruth_[jI], weight);
+	}
+      }
+
+      inFile_p->Close();
+      delete inFile_p;
+    }
 
     outFile_p->cd();
 
@@ -239,17 +352,17 @@ int deriveSampleWeights(std::string inXSectionFileName, std::string inEntriesFil
     delete outFile_p;
   }
   else{
-    outFileName = inEntriesFileName.substr(0, inEntriesFileName.rfind(".txt"));
+    outFileName = inEntriesFileName.substr(0, inEntriesFileName.rfind(txtExt));
     while(outFileName.find("/") != std::string::npos){
       outFileName.replace(0, outFileName.find("/")+1, "");
     }
-    outFileName = "output/" + dateStr + "/" + outFileName + "_DerivedWeights_" + dateStr + ".root";
+    outFileName = "output/" + dateStr + "/" + outFileName + "_DerivedWeights_" + dateStr + rootExt;
   }
 
   std::cout << "FILE, LINE: " << __FILE__ << ", " << __LINE__ << std::endl;
 
-  outFileName.replace(outFileName.rfind(".root"), 5, "");
-  outFileName = outFileName + ".txt";
+  outFileName.replace(outFileName.rfind(rootExt), 5, "");
+  outFileName = outFileName + txtExt;
   
   std::ofstream outFile(outFileName.c_str());
 
